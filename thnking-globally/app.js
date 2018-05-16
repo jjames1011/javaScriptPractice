8a92160f45b0e10963ef9e6eefda8699e47a1f96
Jonathan James <jjamesnwa1011@gmail.com>
2018-05-16 12:18:14 -0700

    added some random buttons

--- thnking-globally/app.js
+++ thnking-globally/app.js
@@ -1,9 +1,38 @@
 //basic event handler setup
+//elements:
 const headerElement = document.getElementById('myHeading');
-const myButton = document.getElementById('myButton');
-const textInput = document.getElementById('myTextInput');
+const bodyElement = document.getElementsByTagName('body')[0];
+//inputs:
+const headerInput = document.getElementById('headerInput');
+const backgroundInput = document.getElementById('backgroundInput');
+//buttons:
+const backgroundButton = document.getElementById('backgroundButton');
+const headlineButton = document.getElementById('headlineButton');
+const randomHeadlineButton = document.getElementById('randomHeadlineColor');
+const randomBackgroundButton = document.getElementById('randomBackgroundColor');
+const randomizeBothButton = document.getElementById('randomizeBoth');
 
-console.log(headerElement);
-myButton.addEventListener('click', () => {
-  headerElement.style.color = textInput.value;
+
+//event handlers :
+const randomRGB = () => {
+  return `rgb(${Math.floor(Math.random()* 255)},${Math.floor(Math.random()* 255)},${Math.floor(Math.random()* 255)})`
+}
+headlineButton.addEventListener('click', () => {
+  headerElement.style.color = headerInput.value;
 });
+
+backgroundButton.addEventListener('click', () => {
+  bodyElement.style.backgroundColor = backgroundInput.value;
+});
+
+randomHeadlineButton.addEventListener('click', () => {
+  headerElement.style.color = randomRGB();
+})
+randomBackgroundButton.addEventListener('click', () => {
+  bodyElement.style.backgroundColor = randomRGB();
+})
+
+randomizeBothButton.addEventListener('click', () => {
+  bodyElement.style.backgroundColor = randomRGB();
+  headerElement.style.color = randomRGB();
+})
